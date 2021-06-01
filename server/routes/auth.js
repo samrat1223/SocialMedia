3ccc4678a59972ace912844cc3ce4a18bdd85744
samrat1223 <samratganguly37@gmail.com>
2021-06-01 16:06:52 +0530

    bcryptjs module added and storing the hashed password rather than original password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,17 +1,42 @@
-const express=require('express');
+const express=require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User  = mongoose.model('User')
+const bcrypt = require('bcryptjs')
 
 
-router.get('/',(req,res) => {
-    res.send("hello")
-})
 
+//signUp route
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body
     if(!email || !password || !name){
        return res.status(422).json({error:"Please add all the fields"})
     }
-    res.json({message:"successfully posted"})
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"Please add all the fields"})
+        }
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+            const user = new User({
+                email, //As email:email both fileds are same so I wrote email only same for password,name
+                password:hashedpassword,
+                name
+            })
+          user.save()
+          .then(user=>{
+              res.json({message:"User saved successfully"})
+          })
+          .catch(err=>{
+              console.log(err);
+          })
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    })  
 })
+
 
 module.exports = router
